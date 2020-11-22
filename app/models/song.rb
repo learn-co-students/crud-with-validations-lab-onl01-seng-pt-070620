504f500f4ef4c70e5c3f82d64c50b5cec47f24ae
keesha <kharg14@gmail.com>
2020-11-21 21:12:43 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,28 +1,31 @@
-class Song < ApplicationRecord
+class Song < ApplicationRecord 
     validates :title, presence: true
-    validates :artist_name, presence: true
-    validate :no_repeated_title
-    validates :released, presence: true
-    validates :release_year, presence: true, unless: :not_released?
-        
-    validate :date_not_possible
+    validates :title, :uniqueness => { :scope => :release_year,
+    :message => "should happen once per year" } 
+    validates_presence_of :release_year, :if => :released? 
 
-    def no_repeated_title
-        if Song.any? {|s| s.title == title && s.artist_name == artist_name && s.release_year == release_year}
-            errors.add(:title, "can't add the same song twice")
+    #validates_presence_of :release_year, unless: -> { :released == false }
+    #validates :release_year, presence: true, unless: -> { :released == false }
+     
+    
+    #validates :release_year, if: -> { :release_year <= Date.today.year }
+    validate  :am_i__future?  
+    #validates :release_year, :exclusion => { :in => :am_i_future? 
+# }
+    validate :not_twice_per_year 
+
+    def am_i__future? 
+       if self.release_year 
+           if self.release_year >= Date.today.year 
+            errors.add(:release_year, "Should not be released in the future") 
+           end
+    end 
+ end
+
+    def not_twice_per_year 
+        if self.release_year 
+            if self 
+            end
         end
     end
-
-    def date_not_possible
-        if release_year.present? && release_year > Date.today.year
-            errors.add(:release_year, "release year can't be in the future")
-        end
-    end
-
-    def not_released?
-        released == false
-    end
-
-        
-
 end

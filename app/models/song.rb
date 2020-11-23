cf540e26a19ae370ae31591a12354fda85a1008f
eulis01 <eulis01@gmail.com>
2020-11-23 12:03:31 -0500

    add validations to Song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,14 @@
 class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: %i[release_year artist_name], message: 'title cannot be repeated the same year by the same artist' }
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  end
+
+  def released?
+    released
+  end
 end

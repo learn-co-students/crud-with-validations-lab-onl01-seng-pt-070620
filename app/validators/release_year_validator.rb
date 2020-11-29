c568368a3eebf0d0fba3adf4af4e7a3d83661d26
github username <jhernandez1692@gmail.com>
2020-11-29 14:08:35 -0500

    Done.

---
+++ app/validators/release_year_validator.rb
@@ -0,0 +1,21 @@
+# class ReleaseYearValidator < ActiveModel::Validator
+
+  
+
+#     def validate(record)
+      
+#       if record.released = true
+       
+        
+#       else
+#         record.errors[:released] << "Release year not validated!"
+#       end
+  
+#     end
+
+    
+
+  
+  
+  
+#   end
---
+++ db/migrate/20201129051058_change_released_to_be_boolean_in_songs.rb
@@ -0,0 +1,5 @@
+class ChangeReleasedToBeBooleanInSongs < ActiveRecord::Migration[5.0]
+  def change
+    change_column :songs, :released, :boolean
+  end
+end
